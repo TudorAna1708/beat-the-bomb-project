28ab15274edb92c92676951ec8938e3ac7f6141f
TudorAna1708 <ana_maria.tudor1708@stud.acs.upb.ro>
2025-05-09 14:28:00 +0300

    changed setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
-from skbuild import setup
-from setuptools import find_packages
+from setuptools import setup, Extension
 
 setup(
     name="bomb_game",
     version="1.0",
     description="Native C extension for Beat the Bomb game",
-    packages=find_packages(),
-    cmake_install_dir="bomb_game",  # spune unde să copieze .pyd-ul
+    ext_modules=[
+        Extension(
+            "bomb_game",
+            sources=["bomb_game.c"],
+            extra_compile_args=["-O2"],  # Optional optimization
+        )
+    ],
 )
