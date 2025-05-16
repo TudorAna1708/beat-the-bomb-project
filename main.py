3dc93af6e916c6e7cc89575f0ae675b3a19e6810
TudorAna1708 <ana_maria.tudor1708@stud.acs.upb.ro>
2025-05-16 16:11:46 +0300

    added smth to main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ DARK_GRAY = (100, 100, 100)
 def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
     try:
-         PyInstaller creates a temp folder and stores path in _MEIPASS
+         #PyInstaller creates a temp folder and stores path in _MEIPASS
          base_path = sys._MEIPASS
     except Exception:
          base_path = os.path.abspath(".")
