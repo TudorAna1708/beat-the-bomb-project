91717ccb735509d0ac812a89eba28da918e68f44
TudorAna1708 <ana_maria.tudor1708@stud.acs.upb.ro>
2025-05-16 15:12:37 +0300

    chg main.py

--- main.py
+++ main.py
@@ -27,11 +27,11 @@ DARK_GRAY = (100, 100, 100)
 
 def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
-    try:
+    #try:
         # PyInstaller creates a temp folder and stores path in _MEIPASS
-        base_path = sys._MEIPASS
-    except Exception:
-        base_path = os.path.abspath(".")
+      #  base_path = sys._MEIPASS
+    #except Exception:
+    base_path = os.path.abspath(".")
 
     return os.path.join(base_path, relative_path)
 
